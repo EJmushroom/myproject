df0a76a8371f91ba6de8d1d8db901f6a1712df67
ej <ej30827@gmail.com>
2019-01-21 14:20:47 +0800

    Java and Kotlin variable data type.

--- src/main/kotlin/com/ej/Hello.kt
+++ src/main/kotlin/com/ej/Hello.kt
@@ -4,6 +4,12 @@ fun main(args: Array<String>) {
 //    println("Hello Kotlin")
     val h=Human()
     h.hello()
+    var age : Int = 19
+    age=20
+    var weight=66.5f
+    var name : String
+    name="Hank"
+
 }
 
 class Human{
